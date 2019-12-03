176a52a67a171da85f196945a2fb0085b9c4fce6
ERUD1T3 <josiasmoukpe@gmail.com>
2019-12-03 16:29:23 -0500

    update

---
+++ src/NeuralNet.py
@@ -0,0 +1,1 @@
+# file to build the neural network for solving the Traveling salesman problem
--- src/dataManip.py
+++ src/dataManip.py
@@ -1,1 +1,6 @@
 # file to handle the data 
+
+
+def readData():
+    '''read the data and convert it into an adjacency graph'''
+    pass
